47b7e6d4c16cd0ac3b1c5609216497f0ce85e1b4
lenanguyen13 <93352336+lenanguyen13@users.noreply.github.com>
2021-11-30 17:28:04 -0600

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,1 +1,8 @@
-myToDo('search and buy room decor, 
+myToDo =['search and buy room decor','write list of assignments due soon','ACT prep','print posters'] 
+print(myToDo)
+print(len(myToDo))
+print(myToDo.index('search and buy room decor'))
+print('myToDo')
+print(f'{len(myToDo)} thing(s) left to do')
+for task in myToDo:
+  print(task)
